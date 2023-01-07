708527537352ec6efd1eb9dff31250aae23bc2c8
fabogit <fabo.dev.email@gmail.com>
2023-01-08 00:41:17 +0100

    create user
    controller & service, module fix

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,6 +1,21 @@
 import { FastifyReply, FastifyRequest } from "fastify";
+import { CreateUserInput } from "./user.schema";
+import { createUser } from "./user.service";
 
 export async function registerUserHandler(
-  request: FastifyRequest,
-  reply: FastifyReply
-) {}
+	request: FastifyRequest<{
+		Body: CreateUserInput;
+	}>,
+	reply: FastifyReply
+) {
+	const body = request.body;
+
+	try {
+		const user = await createUser(body);
+		return reply.code(201).send(user);
+	} catch (error) {
+		// code 409 or 500
+		console.log(error);
+		return reply.code(500).send(error);
+	}
+}
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,12 +3,12 @@ import prisma from "../../utils/prisma";
 import { CreateUserInput } from "./user.schema";
 
 export async function createUser(input: CreateUserInput) {
-	const { password, ...rest } = input;
+	const { password, ...data } = input;
 
 	const { hash, salt } = hashPassword(password);
 
 	const user = await prisma.user.create({
-		data: { ...rest, salt, password: hash },
+		data: { ...data, salt, password: hash },
 	});
 	return user;
 }
