29ae40793d80b7a4b7cde7f7e9aa3c2f9a42a57f
fabogit <fabo.dev.email@gmail.com>
2023-01-07 22:56:50 +0100

    user module
    setup, no data yet

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import Fastify from "fastify";
+import userRoutes from "./modules/user/user.route";
 
 const server = Fastify({ logger: true });
 
 server.get("/healthcheck", async () => {
-  return { status: "OK" };
+	return { status: "OK" };
 });
 
 async function main() {
-  try {
-    await server.listen({ port: 3001, host: "0.0.0.0" });
-    console.log(`✅ Server running`);
-  } catch (error) {
-    //
-    console.error(`❌ `, error);
-    process.exit(1);
-  }
+
+	server.register(userRoutes, { prefix: "api/users" });
+
+	try {
+		await server.listen({ port: 3001, host: "0.0.0.0" });
+		console.log(`✅ Server running`);
+	} catch (error) {
+		//
+		console.error(`❌ Server stopped,`, error);
+		process.exit(1);
+	}
 }
 
 main();
