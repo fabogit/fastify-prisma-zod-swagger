0e37c050631eea8f3f06bdf93f5b5eecaf2108b6
fabogit <fabo.dev.email@gmail.com>
2023-01-07 23:33:09 +0100

    user service
    createUser, zod schema

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,6 +1,7 @@
-import prisma  from "../../utils/prisma";
+import prisma from "../../utils/prisma";
+import { CreateUserInput } from "./user.schema";
 
-export async function createUser(input) {
+export async function createUser(input: CreateUserInput) {
 	const user = await prisma.user.create({
 		data: input,
 	});
