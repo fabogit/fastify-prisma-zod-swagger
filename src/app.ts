de8c63d3183eacedc98bbb99405ed3f671ac0997
fabogit <fabo.dev.email@gmail.com>
2023-01-07 22:41:48 +0100

    fastify server bootstrap
    /healthcheck route

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,20 @@
-console.log("Hello");
+import Fastify from "fastify";
+
+const server = Fastify({ logger: true });
+
+server.get("/healthcheck", async () => {
+  return { status: "OK" };
+});
+
+async function main() {
+  try {
+    await server.listen({ port: 3001, host: "0.0.0.0" });
+    console.log(`✅ Server running`);
+  } catch (error) {
+    //
+    console.error(`❌ `, error);
+    process.exit(1);
+  }
+}
+
+main();
