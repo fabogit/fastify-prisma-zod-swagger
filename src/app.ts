557df9b817cd7e0987dd5fb1e5c87602b7c45b80
fabogit <fabo.dev.email@gmail.com>
2023-01-08 12:41:12 +0100

    added swagger
    swagger @host:port/docs

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,13 @@ import Fastify, { FastifyRequest, FastifyReply } from "fastify";
 // import cors from "@fastify/cors";
 import fjwt from "@fastify/jwt";
 
+import { version } from "../package.json";
 import userRoutes from "./modules/user/user.route";
 import productRoutes from "./modules/product/product.route";
 import { userSchemas } from "./modules/user/user.schema";
 import { productSchemas } from "./modules/product/product.schema";
+import swagger from "@fastify/swagger";
+import swaggerui from "@fastify/swagger-ui";
 
 export const server = Fastify({ logger: true });
 
@@ -16,58 +19,103 @@ export const server = Fastify({ logger: true });
 // });
 
 declare module "fastify" {
-	export interface FastifyInstance {
-		authenticate: any;
-	}
+  export interface FastifyInstance {
+    authenticate: any;
+  }
 }
 
 declare module "@fastify/jwt" {
-	interface FastifyJwt {
-		user: {
-			id: number,
-			name: string,
-			email: string;
-		};
-	}
+  interface FastifyJwt {
+    user: {
+      id: number;
+      name: string;
+      email: string;
+    };
+  }
 }
 
 // JWT
 server.register(fjwt, {
-	secret: "supersecret",
+  secret: "supersecret",
 });
 server.decorate(
-	"authenticate",
-	async (request: FastifyRequest, reply: FastifyReply) => {
-		try {
-			await request.jwtVerify();
-		} catch (error) {
-			return reply.send(error);
-		}
-	}
+  "authenticate",
+  async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (error) {
+      return reply.send(error);
+    }
+  }
 );
 
 // healthcheck
 server.get("/healthcheck", async () => {
-	return { status: "OK" };
+  return { status: "OK" };
 });
 
 async function main() {
-	// schemas
-	for (const schema of [...userSchemas, ...productSchemas]) {
-		server.addSchema(schema);
-	}
+  // schemas
+  for (const schema of [...userSchemas, ...productSchemas]) {
+    server.addSchema(schema);
+  }
 
-	// routes
-	server.register(userRoutes, { prefix: "api/users" });
-	server.register(productRoutes, { prefix: "api/products" });
+  await server.register(swagger, {
+    openapi: {
+      info: {
+        title: "Fastify API",
+        description:
+          "PostgreSQL, Prisma, Fastify and Swagger REST API",
+        version: version,
+      },
+      externalDocs: {
+        url: "https://swagger.io",
+        description: "Find more info here",
+      },
+      servers: [{ url: "http://localhost:3001" }],
+      components: {
+        securitySchemes: {
+          apiKey: {
+            type: "apiKey",
+            name: "apiKey",
+            in: "header",
+          },
+        },
+      },
+      security: [{ apiKey: [] }],
+    },
+  });
 
-	try {
-		await server.listen({ port: 3001, host: "0.0.0.0" });
-		console.log(`✅ Server running`);
-	} catch (error) {
-		console.error(`❌ Server stopped,`, error);
-		process.exit(1);
-	}
+  await server.register(swaggerui, {
+    routePrefix: "/docs",
+    initOAuth: {},
+    uiConfig: {
+      docExpansion: "full",
+      deepLinking: false,
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) {
+        next();
+      },
+      preHandler: function (request, reply, next) {
+        next();
+      },
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+  });
+
+  // routes
+  server.register(userRoutes, { prefix: "api/users" });
+  server.register(productRoutes, { prefix: "api/products" });
+
+  try {
+    await server.listen({ port: 3001, host: "0.0.0.0" });
+    console.log(`✅ Server running`);
+  } catch (error) {
+    console.error(`❌ Server stopped,`, error);
+    process.exit(1);
+  }
 }
 
 main();
